f8d7cdab20d83bcc62d68a14724ba57c1d68d7dc
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-16 23:04:26 +0200

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import jsonPlaceHolder from "../apis/jsonPlaceHolder"
+
+export const fetchPost = () => async dispatch => {
+    const response = await jsonPlaceHolder.get('/posts');
+    dispatch({type: 'FETCH_POST', payload: response})
+};
---
+++ src/components/PostList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
+
+class PostList extends Component {
+
+    componentDidMount() {
+        this.props.fetchPost();
+    }
+
+    render() {
+        return <div>PostList</div>
+    }
+}
+
+export default connect(null, { fetchPost })(PostList);
